58faf70928992d307af3b4df3896f361c1e7feec
Dio Lemos <pedro.devsc@outlook.com>
2023-01-16 06:23:58 -0300

    create data fetching function. function will return a promise that may contain our desired file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [moviesList, setMoviesList] = useState([])
+
+  function fetchMoviesHandler(){
+    fetch('https://swapi.dev/api/films/').then(
+      response=>{
+        return response.json();
+      }
+    ).then(data=>{ setMoviesList(prevResults=>data.results)})
+  }
 
   return (
     <React.Fragment>
@@ -25,7 +21,7 @@ function App() {
         <button>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={moviesList} />
       </section>
     </React.Fragment>
   );
